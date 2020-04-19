592ae5b0ad891673c99bf398f3fb7bb2e47b0ef8
floribe2000 <43866194+floribe2000@users.noreply.github.com>
2020-04-19 22:35:33 +0200

    Update other tests

--- src/test/java/SeasonsTest.java
+++ src/test/java/SeasonsTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.assertEquals;
 
 import de.floribe2000.warships_java.direct.api.ApiBuilder;
 import de.floribe2000.warships_java.direct.api.Region;
+import de.floribe2000.warships_java.direct.api.Status;
 import de.floribe2000.warships_java.direct.seasons.RankedBattlesPlayerStatisticsRequest;
 import de.floribe2000.warships_java.direct.seasons.RankedBattlesPlayersStatistics;
 import de.floribe2000.warships_java.direct.seasons.RankedBattlesSeasons;
@@ -37,27 +38,27 @@ public class SeasonsTest {
 	@Test
 	public void testSeasonsRequest() {
 		RankedBattlesSeasonsRequest request = RankedBattlesSeasonsRequest.createRequest()
-			.region(Region.EU).addSeason(15);
+				.region(Region.EU).addSeason(15);
 		RankedBattlesSeasons result = request.fetch();
 		System.out.println(result);
-		assertEquals("ok", result.getStatus());
+		assertEquals(Status.OK, result.getStatus());
 	}
 
 	@Test
 	public void testSeasonsPlayerStats() {
 		RankedBattlesPlayerStatisticsRequest request = RankedBattlesPlayerStatisticsRequest
-			.createRequest().region(Region.EU).addSeason(15).addAccountId(accountId);
+				.createRequest().region(Region.EU).addSeason(15).addAccountId(accountId);
 		RankedBattlesPlayersStatistics result = request.fetch();
 		System.out.println(result);
-		assertEquals("ok", result.getStatus());
+		assertEquals(Status.OK, result.getStatus());
 	}
 
 	@Test
 	public void testSeasonsShipStats() {
 		RankedBattlesShipsStatisticsRequest request = RankedBattlesShipsStatisticsRequest
-			.createRequest().region(Region.EU).addSeason(15).addAccountId(accountId);
+				.createRequest().region(Region.EU).addSeason(15).addAccountId(accountId);
 		RankedBattlesShipsStatistics result = request.fetch();
 		System.out.println(result);
-		assertEquals("ok", result.getStatus());
+		assertEquals(Status.OK, result.getStatus());
 	}
 }
