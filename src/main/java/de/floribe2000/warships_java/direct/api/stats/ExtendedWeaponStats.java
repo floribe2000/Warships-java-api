967b817d5e65dbea0fdfb79b181e2a7e3f5a2cac
SirLefti <thorben.yzer@web.de>
2020-04-27 11:25:58 +0200

    second try to make weapon stats, shipId is present anyway

--- src/main/java/de/floribe2000/warships_java/direct/api/stats/ExtendedWeaponStats.java
+++ src/main/java/de/floribe2000/warships_java/direct/api/stats/ExtendedWeaponStats.java
@@ -1,31 +1,16 @@
 package de.floribe2000.warships_java.direct.api.stats;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Objects;
 import lombok.Getter;
 
 /**
- * Extended weapon stats container for weapon types that define shots and hits by this type
+ * Container containing extended weapon stats
  *
  * @author SirLefti
  */
 @Getter
-public class ExtendedWeaponStats extends WeaponStats implements IExtendedWeaponStats {
+public class ExtendedWeaponStats extends WeaponStats {
 
-	public static ExtendedWeaponStats mergeExtended(Collection<ExtendedWeaponStats> stats) {
-		ExtendedWeaponStats merged = new ExtendedWeaponStats();
-		stats.stream().filter(Objects::nonNull).forEach(s -> {
-			merged.hits += s.hits;
-			merged.shots += s.shots;
-		});
-		return merged;
-	}
+	private int hits = 0;
+	private int shots = 0;
 
-	public static ExtendedWeaponStats mergeExtended(ExtendedWeaponStats... stats) {
-		return mergeExtended(Arrays.asList(stats));
-	}
-
-	protected int hits = 0;
-	protected int shots = 0;
 }
--- src/main/java/de/floribe2000/warships_java/direct/api/stats/WeaponStats.java
+++ src/main/java/de/floribe2000/warships_java/direct/api/stats/WeaponStats.java
@@ -1,33 +1,16 @@
 package de.floribe2000.warships_java.direct.api.stats;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Objects;
 import lombok.Getter;
 
 /**
- * Base weapon stats container for weapon types that do not define shots and hits by this type
+ * Container containing base weapon stats
  *
  * @author SirLefti
  */
 @Getter
-public class WeaponStats implements IWeaponStats {
+public class WeaponStats {
 
-	public static WeaponStats merge(Collection<WeaponStats> stats) {
-		WeaponStats merged = new WeaponStats();
-		stats.stream().filter(Objects::nonNull).forEach(s -> merged.frags += s.frags);
-		stats.stream().filter(Objects::nonNull).max(Comparator.comparingInt(WeaponStats::getMax_frags_battle)).ifPresent(s -> {
-			merged.max_frags_battle = s.max_frags_battle;
-		});
-		return merged;
-	}
-
-	public static WeaponStats merge(ExtendedWeaponStats... stats) {
-		return merge(Arrays.asList(stats));
-	}
-
-	protected int frags = 0;
-	protected int max_frags_battle = 0;
-
+	private int frags = 0;
+	private int max_frags_battle = 0;
+	private long max_frags_ship_id = 0;
 }
