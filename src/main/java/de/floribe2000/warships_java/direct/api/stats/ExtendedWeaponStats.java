8d4d12f179c8faba17ca51fd30d3029955d41c68
SirLefti <thorben.yzer@web.de>
2020-05-03 16:05:10 +0200

    mergeable WeaponStats

--- src/main/java/de/floribe2000/warships_java/direct/api/stats/ExtendedWeaponStats.java
+++ src/main/java/de/floribe2000/warships_java/direct/api/stats/ExtendedWeaponStats.java
@@ -1,16 +1,39 @@
 package de.floribe2000.warships_java.direct.api.stats;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Objects;
 import lombok.Getter;
 
 /**
- * Container containing extended weapon stats
+ * Extended weapon stats container for all weapon types that count hits and shots.
  *
  * @author SirLefti
  */
 @Getter
 public class ExtendedWeaponStats extends WeaponStats {
 
-	private int hits = 0;
-	private int shots = 0;
+	public static ExtendedWeaponStats mergeExtended(Collection<ExtendedWeaponStats> stats) {
+		ExtendedWeaponStats merged = new ExtendedWeaponStats();
+
+		stats.stream().filter(Objects::nonNull).forEach(s -> {
+			merged.hits += s.hits;
+			merged.shots += s.shots;
+			merged.frags += s.frags;
+		});
+		stats.stream().filter(Objects::nonNull).max(Comparator.comparingInt(WeaponStats::getMax_frags_battle)).ifPresent(s -> {
+			merged.max_frags_battle = s.max_frags_battle;
+			merged.max_frags_ship_id = s.max_frags_ship_id;
+		});
+		return merged;
+	}
+
+	public static ExtendedWeaponStats mergeExtended(ExtendedWeaponStats... stats) {
+		return mergeExtended(Arrays.asList(stats));
+	}
+
+	protected int hits = 0;
+	protected int shots = 0;
 
 }
--- src/main/java/de/floribe2000/warships_java/direct/api/stats/WeaponStats.java
+++ src/main/java/de/floribe2000/warships_java/direct/api/stats/WeaponStats.java
@@ -1,16 +1,34 @@
 package de.floribe2000.warships_java.direct.api.stats;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Objects;
 import lombok.Getter;
 
 /**
- * Container containing base weapon stats
+ * Base weapon stats container for all weapon types.
  *
  * @author SirLefti
  */
 @Getter
 public class WeaponStats {
 
-	private int frags = 0;
-	private int max_frags_battle = 0;
-	private long max_frags_ship_id = 0;
+	public static WeaponStats merge(Collection<WeaponStats> stats) {
+		WeaponStats merged = new WeaponStats();
+		stats.stream().filter(Objects::nonNull).forEach(s -> merged.frags += s.frags);
+		stats.stream().filter(Objects::nonNull).max(Comparator.comparingInt(WeaponStats::getMax_frags_battle)).ifPresent(s -> {
+			merged.max_frags_battle = s.max_frags_battle;
+			merged.max_frags_ship_id = s.max_frags_ship_id;
+		});
+		return merged;
+	}
+
+	public static WeaponStats merge(WeaponStats... stats) {
+		return merge(Arrays.asList(stats));
+	}
+
+	protected int frags = 0;
+	protected int max_frags_battle = 0;
+	protected long max_frags_ship_id = 0;
 }
